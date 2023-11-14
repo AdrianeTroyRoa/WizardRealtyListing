3b329382f1a674307935fe1f58794d9a504ffc05
Adriane <troy@fedora.com>
2023-11-14 12:21:53 +0800

    demo parameter passing

--- app.py
+++ app.py
@@ -1,14 +1,14 @@
-from flask import Flask 
+from flask import Flask, render_template 
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello! <h1>World!</h1>"
+    return render_template("index.html")
 
 @app.route("/<name>")
 def user(name):
-    return "Hello user" + name
+    return render_template("pages.html", page_name=name)
 
 if __name__ == "__main__":
-    app.run()
+    app.run(host="0.0.0.0")
