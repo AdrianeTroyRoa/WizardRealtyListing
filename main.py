7cdc3f5cfc484e6405601a51a6498b6af0560b77
Adriane <troy@fedora.com>
2023-11-16 21:11:18 +0800

    reorganized for app proper

---
+++ main.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template 
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/<name>")
+def user(name):
+    return render_template("pages.html", page_name=name)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
