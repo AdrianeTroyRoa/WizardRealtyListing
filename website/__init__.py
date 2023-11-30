7d8441f0f0a5d8e8362acef7e28c197cb8994b66
Randell Cordova <rancordz@gmail.com>
2023-11-30 12:35:10 +0800

    fixed login required flash

--- website/__init__.py
+++ website/__init__.py
@@ -22,6 +22,7 @@ def create_app():
     login_manager = LoginManager()
     login_manager.login_view = 'auth.login'
     login_manager.init_app(app)
+    login_manager.login_message = ''
 
     @login_manager.user_loader
     def load_user(id):
